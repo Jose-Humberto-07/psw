886ae273c2f0a66273fd98b4824ff9b2fad611fe
jose-humberto-07 <jhpnascimento96@gmail.com>
2025-01-07 23:18:37 -0300

    create controllers in viwes

--- psw202501/assinatura/views/view.py
+++ psw202501/assinatura/views/view.py
@@ -2,7 +2,7 @@ import __init__
 from models.database import engine
 from sqlmodel import Session, select
 from datetime import date
-from models.model import Subscription
+from models.model import Subscription, Payments
 
 
 class SubscriptionService:
@@ -23,6 +23,36 @@ class SubscriptionService:
             results = sesion.exec(statement).all()
 
             return results
+        
+    def _has_pay(self, results):
+        for result in results:
+            if result.date.month == date.today().month:
+                return True
+        return False 
+
+        
+    def pay(self, subscription: Subscription):
+        with Session(self.engine) as session:
+            statement = select(Payments).join(Subscription).where(Subscription.empresa==subscription.empresa)
+            results = session.exec(statement).all()
+            if self._has_pay(results):
+                question = input('Essa conta já foi paga esse mês, de')
+            
+                if not question.upper() == 'Y':
+                    return
+
+            pay = Payments(subscription_id=subscription.id)
+            session.add(pay)
+            session.commit()
+
+    def total_value(self):
+        with Session(self.engine) as session:
+            statement = select(Subscription)
+            results = session.exec(statement).all()
+        total = 0
+        for result in results:
+            total += result.valor
+        return float(total)
 
 ss = SubscriptionService(engine)
 
